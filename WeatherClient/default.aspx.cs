8bd24292237fec950df21739d9394d380f5478a2
Martin Raldholm <martin@raldholm.se>
2016-01-12 12:32:34 +0100

    Finished Weather service + client

---
+++ WeatherClient/default.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WeatherClient.WeatherService;
+
+namespace WeatherClient
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            WeatherService.WeatherServiceSoapClient client = new WeatherServiceSoapClient();
+
+            TextBox1.Text = client.Weather();
+
+        }
+    }
+}
