285284aa50aebbf3aea01369f2f8e8b8cd93ec29
Martin Raldholm <martin@raldholm.se>
2016-01-12 12:41:11 +0100

    Added add2numbersService

---
+++ Add2NumbersService/Add2NumbersService.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="Add2NumbersService.asmx.cs" Class="Add2NumbersService.Add2NumbersService" %>
---
+++ Add2NumbersService/Add2NumbersService.asmx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace Add2NumbersService
+{
+    /// <summary>
+    /// Summary description for Add2NumbersService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class Add2NumbersService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public int Add2Numbers(int tal1, int tal2)
+        {
+            return tal1 + tal2;
+        }
+    }
+}
